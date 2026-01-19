c03c718cda3585b1e1cba358740a04edf3962ffc
mothcolada <mothcolada@gmail.com>
2026-01-19 16:46:48 -0500

    ok

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import sqlite3
 
 
 # TODO: use database instead of json for nami feeds
-# TODO: add more characters to calendar (1/16 and later, oh my god please just do it it's so soon)
+# TODO: add more characters to calendar
 
 # conn = sqlite3.connect('database.db')
 # cursor = conn.cursor()
@@ -30,7 +30,7 @@ class Bot(commands.Bot):
         self.is_nougat = self.user.id == 1425561875885719634  # could be either Nougat or my test bot miscolada
         await self.load_extension('daily_character')
         await self.load_extension('nami_feeds')
-        await self.load_extension('refresh_frantically')
+        # await self.load_extension('refresh_frantically')
 
 
     async def on_ready(self):
