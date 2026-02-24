a14cba665926a35083c093cb3c869c63d71cc919
--local <--local>
2026-02-24 09:52:26 -0500

    i have made a grave error

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ async def main():
         ) as bot,
     ):
         discord.utils.setup_logging()
-        await bot.start(os.environ["NOUGAT_TOKEN"])
+        await bot.start(os.environ["TOKEN"])
 
 
 if __name__ == "__main__":
