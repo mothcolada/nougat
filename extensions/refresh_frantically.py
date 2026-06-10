5e167f51b0f2577c48639153fe52335b21b46dbd
mothcolada <mothcolada@gmail.com>
2026-06-10 00:25:31 -0400

    no longer refresh

--- extensions/refresh_frantically.py
+++ extensions/refresh_frantically.py
@@ -15,14 +15,15 @@ class RefreshFrantically(commands.Cog):
 
     @tasks.loop(seconds=10.0)
     async def refresh(self):
-        if self.bot.is_ready():
-            response = requests.get(f"https://nomnomnami.itch.io/week-with-timber?password={os.environ['WWT_PASSWORD']}", headers={"Cookie": os.environ['WWT_COOKIE']})
-            soup = BeautifulSoup(response.content, 'html.parser')
-            timestamp = soup.find('div', {'class': 'update_timestamp'})
-            if not timestamp:
-                return
-            if timestamp.find('abbr')['title'] != '17 April 2026 @ 20:03 UTC':
-                await self.bot.get_channel(1074754885070897202).send('<@422162909582589963> we might be back ' + str(response.status_code))
+        return
+        # if self.bot.is_ready():
+        #     response = requests.get(f"https://nomnomnami.itch.io/week-with-timber?password={os.environ['WWT_PASSWORD']}", headers={"Cookie": os.environ['WWT_COOKIE']})
+        #     soup = BeautifulSoup(response.content, 'html.parser')
+        #     timestamp = soup.find('div', {'class': 'update_timestamp'})
+        #     if not timestamp:
+        #         return
+        #     if timestamp.find('abbr')['title'] != '10 June 2026 @ 00:15 UTC':
+        #         await self.bot.get_channel(1074754885070897202).send('<@422162909582589963> we might be back ' + str(response.status_code))
     
 
 async def setup(bot: commands.Bot):
